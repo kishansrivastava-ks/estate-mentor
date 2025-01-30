3c9895a51c583d8b79c9cdc87e481977ae75ff9f
Kishan Srivastava <kishansrivastava.01.ks@gmail.com>
2025-01-30 16:36:24 +0530

    futher components added on landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,9 @@ import HeroSection from "../components/Home/HeroSection";
 import FeaturesSection from "../components/Home/FeaturesSection";
 import StatsSection from "../components/Home/StatsSection";
 import WhyUsSection from "../components/Home/WhyUsSection";
+import ConsultationCTA from "../components/Home/ConsultationCTA";
+import MissionSection from "../components/Home/MissionSection";
+import TestimonialsSection from "../components/Home/TestimonialsSection";
 
 const Container = styled.div`
   /* border: 1px solid white; */
@@ -22,6 +25,9 @@ function Home() {
       <FeaturesSection />
       <StatsSection />
       <WhyUsSection />
+      <ConsultationCTA />
+      <MissionSection />
+      <TestimonialsSection />
     </>
   );
 }
